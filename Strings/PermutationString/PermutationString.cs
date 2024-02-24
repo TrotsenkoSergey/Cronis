10c56aa8bdb4a06e93550bb1f90bc8ebcc24a374
Sergey Trotsenko <advocate.tsv@gmail.com>
2024-02-24 21:16:47 +0600

    feat(Strings): add permutation and replace space tasks

---
+++ Strings/PermutationString/PermutationString.cs
@@ -0,0 +1,27 @@
+﻿namespace Strings;
+
+public class PermutationString
+{
+    public bool IsPermutated(string s1, string s2)
+    { 
+        if (s1.Length != s2.Length) return false;
+
+        int[] letters = new int[128]; // ASCII
+
+        foreach (char letter in s1.ToCharArray())
+        {
+            int letterCode = (int)letter;
+            letters[letterCode]++;
+        }
+
+        foreach (char letter in s2.ToCharArray()) 
+        { 
+            int letterCode = (int) letter;
+            letters[letterCode]--;
+            if (letters[letterCode] == -1) return false;
+        }
+
+        return true;
+    }
+
+}
---
+++ Strings/ReplaceSpaces/ReplaceSpacesTask.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Strings;
+
+public class ReplaceSpacesTask
+{
+    public string ReplaceSpaces(string s, int length)
+    {
+        StringBuilder sb = new();
+        int i = 0;
+        for (i = 0; i < length; i++)
+        {
+            if (char.IsWhiteSpace(s[i])) 
+                sb.Append("%20");
+            else 
+                sb.Append(s[i]);
+        }
+        return sb.ToString();
+    }
+}
