89dc567132a85041b77d89c378eefcf8fae0ad72
Sergey Trotsenko <advocate.tsv@gmail.com>
2024-02-25 22:16:39 +0600

    feat(Strings): add palindrom task

---
+++ Strings/PalindromString/PalindromString.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Strings;
+
+public class PalindromString
+{
+    public bool IsPermutationIfPalindrome(string str)
+    {
+        bool[] lettersTable = new bool[26]; // всего 26 букв английского алфавита
+        int oddLettersCount = 0;
+        foreach (char letter in str) 
+        { 
+            if (letter == ' ') continue;
+            int letterCode = GetLetterCode(letter); // получим код в пределах от 0 до 26
+
+            lettersTable[letterCode] = !lettersTable[letterCode];
+
+            if (lettersTable[letterCode]) oddLettersCount++;
+            else oddLettersCount--;
+        }
+        return oddLettersCount <= 1; // т.к. палиндром может быть как с четным количеством букв (т.е. 0)
+                                     // так и с нечетным (т.е. 1)   
+                                     // все остальное > 1 - не палиндром
+    }
+
+    private int GetLetterCode(char letter)
+    {
+        int aCode = 'a';
+        int zCode = 'z';
+        int letterCode = letter;
+        if (letterCode < aCode || letterCode > zCode) 
+        {
+            throw new IndexOutOfRangeException("The letter is not in range from 'a' to 'z'.");
+        }
+
+        return letterCode - aCode;
+    }
+}
--- Strings/ReplaceSpaces/ReplaceSpacesTask.cs
+++ Strings/ReplaceSpaces/ReplaceSpacesTask.cs
@@ -4,7 +4,35 @@ namespace Strings;
 
 public class ReplaceSpacesTask
 {
-    public string ReplaceSpaces(string s, int length)
+    public string ReplaceSpacesWrapper(string str, int trueLength)
+    {
+        char[] strAsArr = str.ToCharArray();
+        ReplaceSpaces(strAsArr, trueLength);
+        return new string(strAsArr);
+    }
+
+    private void ReplaceSpaces(char[] strAsArr, int trueLength)
+    {
+        int tailLetterIndex = strAsArr.Length;
+        for (int i = trueLength - 1; i != 0; i--) 
+        {
+            int phraseLetterIndex = i;
+            if (strAsArr[phraseLetterIndex] != ' ')
+            {
+                strAsArr[tailLetterIndex - 1] = strAsArr[phraseLetterIndex];
+                tailLetterIndex--;
+            }
+            else 
+            {
+                strAsArr[tailLetterIndex - 1] = '0';
+                strAsArr[tailLetterIndex - 2] = '2';
+                strAsArr[tailLetterIndex - 3] = '%';
+                tailLetterIndex -= 3;
+            }
+        }
+    }
+
+    public string ReplaceSpacesMy(string s, int length)
     {
         StringBuilder sb = new();
         int i = 0;
